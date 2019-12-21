d7f808f963a4c5cce0a3b0cd9cdefe91778197c6
justinsadakhom <justin.sadakhom@gmail.com>
2019-12-21 18:42:07 -0500

    Fix missing attributes header

--- sample/client.py
+++ sample/client.py
@@ -4,10 +4,11 @@ from sample.business import Business
 
 class Client(Business):
     """
-    budget: how much the client is willing to spend, in dollars,
-            formatted as a Tuple expressing a range of (min, max)
-    materials: what materials the client is seeking
-    delivery_time: estimated time they want their order fulfilled by
+    Attributes:
+        budget: how much the client is willing to spend, in dollars,
+                formatted as a Tuple expressing a range of (min, max)
+        materials: what materials the client is seeking
+        delivery_time: estimated time they want their order fulfilled by
     """
 
     def __init__(self, budget: Tuple[int], materials: List[str],
