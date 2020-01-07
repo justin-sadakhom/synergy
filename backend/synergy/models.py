fcaf541310727a13983bb1758a1db54312f150d2
justinsadakhom <justin.sadakhom@gmail.com>
2020-01-07 17:46:13 -0500

    Add docstring to models
    Also made a tweak Product.quality(). Adjusted max_digits to have a
    correct value of 2 since a scale from 0.0 to 5.0 will never require
    more than 2 digits.

--- backend/synergy/models.py
+++ backend/synergy/models.py
@@ -4,18 +4,23 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Product(models.Model):
+    """ A commodity that a Supplier wants to sell.
+
+        Attributes:
+            :name: Category it belongs to.
+            :quantity: How much is available for a single order.
+            :quality: Quality rating, from a scale of 0.0 to 5.0.
+            :delivery_time: How long it takes to ship, in days.
+            :cost: Cost per unit, in dollars.
+    """
 
     name = models.CharField(max_length=50)
-
     quantity = models.IntegerField(validators=[MinValueValidator(0)])
-
     quality = models.DecimalField(
         default=0,
-        max_digits=3,
+        max_digits=2,
         decimal_places=1,
         validators=[MinValueValidator(0), MaxValueValidator(5)]
     )
-
     delivery_time = models.IntegerField()
-
     cost = models.DecimalField(max_digits=None, decimal_places=2)
