b95100349144cf9938694138d27e5722a642172c
justinsadakhom <justin.sadakhom@gmail.com>
2020-05-06 15:05:32 -0400

    Add forgot password feature

--- backend/synergy/urls.py
+++ backend/synergy/urls.py
@@ -1,12 +1,11 @@
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     # path('forms/product/', views.submit_product),
     # path('forms/request/', views.submit_request),
-    path('register/', views.register, name='register'),
-    path('login/', views.CustomLoginView.as_view(template_name='synergy/register.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('home/', views.home, name='home'),
+    path('accounts/login/', views.CustomLoginView.as_view(redirect_authenticated_user=True, template_name='synergy/register.html'), name='login'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
