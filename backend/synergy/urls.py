c22667dfc88ea08ef70d7a115e2b245b234b9a2f
justinsadakhom <justin.sadakhom@gmail.com>
2020-05-11 21:46:47 -0400

    Split registration and login pages
    CSS for the registration page is still non functional.

--- backend/synergy/urls.py
+++ backend/synergy/urls.py
@@ -6,6 +6,13 @@ urlpatterns = [
     # path('forms/product/', views.submit_product),
     # path('forms/request/', views.submit_request),
     path('home/', views.home, name='home'),
-    path('accounts/login/', views.CustomLoginView.as_view(redirect_authenticated_user=True, template_name='synergy/register.html'), name='login'),
+    path(
+        'accounts/login/',
+        views.CustomLoginView.as_view(
+            redirect_authenticated_user=True,
+            template_name='synergy/login.html'),
+        name='login'
+    ),
+    path('accounts/register/', views.register, name='register'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
